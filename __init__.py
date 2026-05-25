347d7e6c46226403a2821c712d208d0dd3bff1f2
antogarper <102247886+antogarper@users.noreply.github.com>
2026-05-25 23:24:34 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-import streamlit.components.v1 as components
-import os
-
-_FRONTEND_DIR = os.path.join(os.path.dirname(__file__), "frontend")
-
-physio_form = components.declare_component(
-    "physio_form",
-    path=_FRONTEND_DIR
-)
